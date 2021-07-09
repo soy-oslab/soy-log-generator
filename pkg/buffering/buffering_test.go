e0188649578f54572b9c1429eb6a5092ae526300
BlaCkinkGJ <ss5kijun@gmail.com>
2021-07-09 12:48:51 +0900

    refactor(buffering): remove the ambiguous part in the test file
    `buffering_test.go` file has too ambiguous when I see that file.
    Exactly, in terms of the `buffering` does.
    
    It's too complex to understand that buffering is the packages buffering
    is or not. So, I fixed of it. And this commit describes it

--- pkg/buffering/buffering_test.go
+++ pkg/buffering/buffering_test.go
@@ -16,7 +16,7 @@ func setup(prefix string) (*buffering.Buffering, error) {
 	if err != nil {
 		log.Fatalf("test file creation failed: %v", err)
 	}
-	buffering, err := buffering.NewBuffering(testFile.Name(), func(str string, args interface{}) error {
+	b, err := buffering.NewBuffering(testFile.Name(), func(str string, args interface{}) error {
 		s, ok := args.([]interface{})
 		if !ok {
 			log.Println(strings.Trim(str, "\n"), s)
@@ -25,25 +25,25 @@ func setup(prefix string) (*buffering.Buffering, error) {
 		}
 		return nil
 	})
-	return buffering, err
+	return b, err
 }
 
-func teardown(buffering *buffering.Buffering) {
-	buffering.Close()
-	os.Remove(buffering.GetFile().Name())
+func teardown(b *buffering.Buffering) {
+	b.Close()
+	os.Remove(b.GetFile().Name())
 }
 
 func TestNewBufferingSuccess(t *testing.T) {
-	buffering, err := setup("test-new-buffering-success")
-	defer teardown(buffering)
-	if err != nil || buffering == nil {
+	b, err := setup("test-new-buffering-success")
+	defer teardown(b)
+	if err != nil || b == nil {
 		t.Errorf("buffering generation failed %v", err)
 	}
 }
 
 func TestNewBufferingFailed_InvalidFileName(t *testing.T) {
-	buffering, err := buffering.NewBuffering("", func(str string, args interface{}) error { return nil })
-	if buffering != nil && err == nil {
+	b, err := buffering.NewBuffering("", func(str string, args interface{}) error { return nil })
+	if b != nil && err == nil {
 		t.Errorf("invalid file name test failed %v", err)
 	}
 }
@@ -55,62 +55,62 @@ func TestNewBufferingFailed_NilFunction(t *testing.T) {
 	}
 	defer testFile.Close()
 	defer os.Remove(testFile.Name())
-	buffering, err := buffering.NewBuffering(testFile.Name(), nil)
-	if buffering != nil && err == nil {
+	b, err := buffering.NewBuffering(testFile.Name(), nil)
+	if b != nil && err == nil {
 		t.Errorf("nil function test failed %v", err)
 	}
 }
 
 func TestGetFileSize(t *testing.T) {
-	buffering, _ := setup("test-get-file-size")
-	defer teardown(buffering)
+	b, _ := setup("test-get-file-size")
+	defer teardown(b)
 
-	_, err := buffering.GetFileSize()
+	_, err := b.GetFileSize()
 	if err != nil {
 		t.Errorf("get file size failed %v", err)
 	}
 }
 
 func TestFileSizeChanged(t *testing.T) {
-	buffering, _ := setup("test-file-size-changed")
-	defer teardown(buffering)
+	b, _ := setup("test-file-size-changed")
+	defer teardown(b)
 
-	targetFile, _ := os.OpenFile(buffering.GetFile().Name(), os.O_WRONLY|os.O_CREATE, 0755)
+	targetFile, _ := os.OpenFile(b.GetFile().Name(), os.O_WRONLY|os.O_CREATE, 0755)
 	defer targetFile.Close()
 	n, err := targetFile.WriteString("01234567890123456789\n")
 	if err != nil || n == 0 {
 		log.Fatalf("testFile write failed %v\n", err)
 	}
 
-	buffering.GetFile().Seek(0, io.SeekEnd)
-	if v, _ := buffering.IsValidFileSize(); !v {
+	b.GetFile().Seek(0, io.SeekEnd)
+	if v, _ := b.IsValidFileSize(); !v {
 		t.Errorf("is valid but return is invalid")
 	}
 	// underflow location move to end
-	buffering.GetFile().Seek(0, io.SeekStart)
-	if v, _ := buffering.IsValidFileSize(); !v {
+	b.GetFile().Seek(0, io.SeekStart)
+	if v, _ := b.IsValidFileSize(); !v {
 		t.Errorf("is valid but return is invalid")
 	}
-	buffering.GetFile().Seek(1, io.SeekEnd)
-	if v, _ := buffering.IsValidFileSize(); v {
+	b.GetFile().Seek(1, io.SeekEnd)
+	if v, _ := b.IsValidFileSize(); v {
 		t.Errorf("is invalid but return is valid")
 	}
 }
 
 func TestUpdateToValidOffset(t *testing.T) {
-	buffering, _ := setup("test-update-to-valid-offset")
-	defer teardown(buffering)
-	targetFile, _ := os.OpenFile(buffering.GetFile().Name(), os.O_WRONLY|os.O_CREATE, 0755)
+	b, _ := setup("test-update-to-valid-offset")
+	defer teardown(b)
+	targetFile, _ := os.OpenFile(b.GetFile().Name(), os.O_WRONLY|os.O_CREATE, 0755)
 	defer targetFile.Close()
 	n, err := targetFile.WriteString("01234567890123456789\n")
 	if err != nil || n == 0 {
 		log.Fatalf("testFile write failed %v\n", err)
 	}
 
-	target, _ := buffering.GetFile().Seek(0, io.SeekEnd)
-	buffering.GetFile().Seek(1, io.SeekEnd)
-	buffering.UpdateToValidOffset()
-	current, _ := buffering.GetFile().Seek(0, io.SeekCurrent)
+	target, _ := b.GetFile().Seek(0, io.SeekEnd)
+	b.GetFile().Seek(1, io.SeekEnd)
+	b.UpdateToValidOffset()
+	current, _ := b.GetFile().Seek(0, io.SeekCurrent)
 	if current != target {
 		t.Errorf("update to valid offset is failed %v->%v", current, target)
 	}
@@ -130,42 +130,42 @@ func writeFiles(filename string) []string {
 }
 
 func TestDoReadLines(t *testing.T) {
-	buffering, _ := setup("test-do-read-lines")
-	defer teardown(buffering)
-	stringList := writeFiles(buffering.GetFile().Name())
+	b, _ := setup("test-do-read-lines")
+	defer teardown(b)
+	stringList := writeFiles(b.GetFile().Name())
 
 	i := 0
-	buffering.SetProcessingFunction(func(str string, _ interface{}) error {
+	b.SetProcessingFunction(func(str string, _ interface{}) error {
 		if strings.Compare(str, stringList[i]) != 0 {
 			t.Errorf("string is not equal %s <> %s", str, stringList[i])
 		}
 		i++
 		return nil
 	})
-	_, err := buffering.DoReadLines()
+	_, err := b.DoReadLines()
 	if err != nil {
 		t.Errorf("do readlines failed %v", err)
 	}
 }
 
 func TestDoReadLinesNil(t *testing.T) {
-	buffering, _ := setup("test-do-read-lines-nil")
-	defer teardown(buffering)
-	buffering.SetProcessingFunction(nil)
-	_, err := buffering.DoReadLines()
+	b, _ := setup("test-do-read-lines-nil")
+	defer teardown(b)
+	b.SetProcessingFunction(nil)
+	_, err := b.DoReadLines()
 	if err == nil {
 		t.Errorf("do readlines nil function failed %v", err)
 	}
 }
 
 func TestDoReadLinesInvalidFunction(t *testing.T) {
-	buffering, _ := setup("test-do-read-lines-invalid-function")
-	defer teardown(buffering)
-	_ = writeFiles(buffering.GetFile().Name())
-	buffering.SetProcessingFunction(func(_ string, _ interface{}) error {
+	b, _ := setup("test-do-read-lines-invalid-function")
+	defer teardown(b)
+	_ = writeFiles(b.GetFile().Name())
+	b.SetProcessingFunction(func(_ string, _ interface{}) error {
 		return errors.New("sample error")
 	})
-	_, err := buffering.DoReadLines()
+	_, err := b.DoReadLines()
 	if err == nil {
 		t.Errorf("do readlines invalid function %v", err)
 	}
